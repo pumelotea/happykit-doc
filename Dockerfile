0d358b6119a38f23da680010016406dbe3c46f16
pumelotea <535553297@qq.com>
2020-12-25 15:04:39 +0800

    update

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:1.15
-RUN mkdir /html
-COPY docs/.vuepress/dist/  /html
-COPY nginx.conf /etc/nginx/nginx.conf
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   title: 'VueDocs',
-  base: '/happykit-doc/',
   description: 'v1.0',
   head: [
     ['link', { rel: 'icon', href: '/vuejs.png' }]
@@ -14,59 +13,12 @@ module.exports = {
     sidebarDepth:3,
     nav: [
       {
-        text: "HappyBoot",
-        link: "/develop/",
+        text: "happykit",
+        link: "/happykit/",
       },
-      {
-        text: "ElementUI",
-        link: "https://element.eleme.cn/#/zh-CN",
-      },
-      {
-        text: "ElementPlus",
-        link: "https://element-plus.org/#/zh-CN",
-      },
-      {
-        text: "MandMobile",
-        link: "https://didi.github.io/mand-mobile/#/zh-CN/home",
-      },
-      {
-        text: "Vue3",
-        link: "https://www.vue3js.cn/",
-      },
-      {
-        text: "EchartsGallery",
-        link: "https://www.makeapie.com/explore.html",
-      },
-      {
-        text: "Echarts",
-        link: "https://echarts.apache.org/zh/index.html",
-      },
-      {
-        text: "Remixicon",
-        link: "https://remixicon.com/",
-      },
-      {
-        text: "Sass",
-        link: "https://www.sass.hk/docs/",
-      },
-      {
-        text: "NPM",
-        link: "https://www.npmjs.com/",
-      },
-      {
-        text: "Drone",
-        link: "https://www.drone.io/",
-      }
     ],
     sidebar: [
-      '/web/',
-      '/develop/',
-      '/specification/',
-      '/tools/',
-      '/happyboot-next/',
-      // '/javascript/',
-      // '/typescript/',
-      // '/node-sass/'
+      '/happykit/',
     ]
   }
 }
